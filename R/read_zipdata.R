6ec42fa4d5289595cfdc3cfc780f3834a47328fa
jeffeaton <jeffrey.eaton@imperial.ac.uk>
2017-10-31 16:02:54 +0200

    update read_zipdata() to handle file not found

--- R/read_zipdata.R
+++ R/read_zipdata.R
@@ -2,7 +2,14 @@
 read_zipdata <- function(zfile, pattern=".dta$", readfn=foreign::read.dta, ignore.case=TRUE, ...){
   tmp <- tempfile()
   on.exit(unlink(tmp))
-  return(readfn(unzip(zfile, grep(pattern, unzip(zfile, list=TRUE)$Name, ignore.case, value=TRUE), exdir=tmp), ...))
+  file <- grep(pattern, unzip(zfile, list=TRUE)$Name, ignore.case, value=TRUE)
+  if(!length(file)){
+    warning(paste0("File name matching pattern '", pattern, "' not found in zip file '", basename(zfile), "'."))
+    return(invisible(NULL))
+  }
+  if(length(file) > 1)
+    warning(paste0("Multiple file names match pattern '", pattern, "' in zip file '", basename(zfile), "'. Returning file '", file[1], "'."))
+  return(readfn(unzip(zfile, file[1], exdir=tmp), ...))
 }
 
 read_zipdta <- function(zfile, ...){
