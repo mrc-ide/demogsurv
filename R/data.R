92560918052d920e5068b01c8c1b69f050a70c09
Charlton Callender <chacalle@uw.edu>
2021-07-14 15:22:10 -0700

    document package datasets

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' DHS Model Individual Recode Dataset
+#'
+#' An example DHS individual recode dataset with each row representing an
+#' individual eligible for the women's questionnaire. This data is not from any
+#' actual DHS survey.
+#'
+#' @format A data frame with 8,348 rows and 819 variables. To keep the model
+#' dataset small only variables starting with 'caseid', 'v0', 'v1', 'v2', 'b',
+#' or 'mm' have been included.
+#'
+#' @source \url{https://dhsprogram.com/data/Download-Model-Datasets.cfm}
+"zzir"
+
+#' DHS Model Births Recode Dataset
+#'
+#' An example DHS births recode dataset with each row representing a child ever
+#' born to individuals eligible for the women's questionnaire. This data is not
+#' from any actual DHS survey.
+#'
+#' @format A data frame with 23,666 rows and 105 variables. To keep the model
+#' dataset small only variables starting with 'caseid', 'v0', 'v1', or 'b',
+#' have been included.
+#'
+#' @source \url{https://dhsprogram.com/data/Download-Model-Datasets.cfm}
+"zzbr"
